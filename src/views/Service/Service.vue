f0fe02ca8e083b55723ab91542852813a2de4174
young <syoh0313@yonsei.ac.kr>
2023-07-03 00:21:20 +0900

    feat : bottom sheet

--- src/views/Service/Service.vue
+++ src/views/Service/Service.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
+    <button type="button" @click="up">button</button>
+    <div class="bottom-sheet" :class="{ 'open': open }">
+      <!-- Bottom Sheet 내용 -->
+      bottom sheet
+    </div>
     <ServiceOptionVue />
     <ServiceActivePanelVue />
     <ServiceDetailVue />
+    
     
   </div>
 
@@ -15,16 +21,38 @@ import ServiceDetailVue from '@/components/Service/Service/ServiceDetail.vue'
 import ServiceActivePanelVue from '@/components/Service/Service/ServiceActivePanel.vue'
 
 export default {
-  name: 'Service',
+  name: 'ServiceContainer',
   components: {
     ServiceOptionVue,
     ServiceDetailVue,
     ServiceActivePanelVue
+  },
+  data(){
+    return{
+      open : false
+    }
+  },
+  methods: {
+    up(){
+      this.open = !this.open
+    }
   }
 
 }
 </script>
 
 <style>
+.bottom-sheet {
+  position: fixed;
+  bottom: -100%; /* 시작 위치 */
+  left: 0;
+  width: 100%;
+  height: 300px; /* Bottom Sheet의 높이 설정 */
+  background-color: #f0f0f0;
+  transition: bottom 0.3s ease-in-out;
+}
 
+.bottom-sheet.open {
+  bottom: 0; /* 열렸을 때의 위치 */
+}
 </style>
