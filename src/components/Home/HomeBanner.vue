cc772e915f5cbb7bca561c9df1eae7926d33bde0
seoyamin <seoyamin@ewhain.net>
2023-07-11 23:57:40 +0900

    feat : Home - Banner

--- src/components/Home/HomeBanner.vue
+++ src/components/Home/HomeBanner.vue
@@ -1,21 +1,62 @@
 <template>
   <div class="home-banner-container">
-    <img src="@/assets/bannertest.jpg"/>
+    <div class="home-banner-slider">
+      <carousel :items-to-show="1" :wrap-around="true" :autoplay="3000">
+        <slide v-for="img in imgList" :key="img.src">
+          <div class="home-banner-carousel_item">
+            <figure>
+              <img :src="img.src" />
+            </figure>
+          </div>
+        </slide>
+
+        <template #addons>
+          <navigation />
+          <pagination />
+        </template>
+      </carousel>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
+export default {
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation
+  },
+
+  data() {
+    return {
+      imgList: [
+        { src: require("@/assets/home/banner/img_banner_1.png") },
+        { src: require("@/assets/home/banner/img_banner_2.png") },
+        { src: require("@/assets/home/banner/img_banner_3.png") },
+      ]
+    }
+    
+  }
 }
 </script>
 
 <style>
 .home-banner-container {
-  display: flex;
-  flex-direction: column;
   width: 100%;
   justify-content: space-around;
-  background-color: rgb(200, 253, 160);
+}
+.home-banner-carousel_item {
+  width: 100%;
+}
+.home-banner-carousel_item figure {
+  margin: 0;
+}
+.home-banner-carousel_item img {
+  width: 100%;
+  object-fit: scale-down;
 }
 </style>
